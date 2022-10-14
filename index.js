d40973c0dcc62c4d3302b2644be3cc1a3390cd4c
Soumya Ranjan Parida <soumyafc812@gmail.com>
2022-10-14 19:07:22 +0530

    successfully setup the express the router

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 //by default live sever works on port 80
 const port = 8000;
+//use express router
+app.use('/', require('./routes/index'));
+
+
 
 app.listen(port, function (err){
     if (err)
